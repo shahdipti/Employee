45a930a120b104e76a71b57554dfc31579dcfdad
14193 <14193@LAPTOP-BPTGD0N3.home>
2020-03-04 21:33:36 -0500

    commit by dipti

---
+++ Employee/src/pages/EmployeeDetails.java
@@ -0,0 +1,20 @@
+package pages;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Dipti Shah";
+		String address = "LewisCenter";
+		int contact = 123456;
+		System.out.println("The name of the employee is " + name);
+		System.out.println(name+ " lives in " + address);
+		System.out.println(name + " contact is " + contact);
+		
+				
+		
+		
+
+	}
+
+}
