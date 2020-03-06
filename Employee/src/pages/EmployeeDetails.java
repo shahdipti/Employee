4821d1d7b9f63af830edd3fb2d1eb8a694ab31e9
shahdipti <61599173+shahdipti@users.noreply.github.com>
2020-03-06 16:10:04 -0500

    Update EmployeeDetails.java

--- Employee/src/pages/EmployeeDetails.java
+++ Employee/src/pages/EmployeeDetails.java
@@ -10,7 +10,7 @@ public class EmployeeDetails {
 		System.out.println("The name of the employee is " + name);
 		System.out.println(name+ " lives in " + address);
 		System.out.println(name + " contact is " + contact);
-		// this is practice
+		// this is my first project
 
 
 		
